13333b677ab0ccfcfe95a4600d4963c23705d998
shoaib <shoaibhossain24@gmail.com>
2023-08-09 11:28:19 +0600

    add bmi calculator

--- calculator.js
+++ calculator.js
@@ -8,13 +8,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
 
 app.post("/", function (req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
+
   var result = num1 + num2;
 
-  res.send("Your number has been added yoo : " + result);
+  res.send("The result of the calculation is " + result);
+});
+
+app.post("/bmicalculator", function (req, res) {
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+
+  var result = weight / (height * height);
+
+  res.send("Your BMI is " + result);
 });
 
 app.listen(3000, function () {
